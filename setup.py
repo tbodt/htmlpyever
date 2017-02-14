75af8859d2a447166b069f40ba83f2428520116f
Theodore Dubois <tblodt@icloud.com>
2017-02-14 13:18:42 -0800

    Add `cargo build --release` to build_ext

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
+import subprocess
 from setuptools import setup, Extension
-from Cython.Distutils import build_ext
+from setuptools.command.build_ext import build_ext as setuptools_build_ext
+from Cython.Build.Cythonize import cythonize
 import lxml
 
+class build_ext(setuptools_build_ext):
+    def build_extension(self, ext):
+        subprocess.check_call(['cargo', 'build', '--release'])
+        setuptools_build_ext.build_extension(self, ext)
+
+includes = ['/usr/include/libxml2'] + lxml.get_include()
 setup(
     name='htmlpyever',
 
-    ext_modules=[Extension(
+    ext_modules=cythonize([Extension(
         name='htmlpyever',
         sources=['htmlpyever.pyx'],
         libraries=['html5ever_glue', 'xml2'],
         library_dirs=['target/release'],
-        include_dirs=['.', '/usr/include/libxml2'] + lxml.get_include(),
-    )],
+        include_dirs=includes,
+    )], include_path=includes),
 
     setup_requires=['cython'],
     install_requires=['lxml'],
-    cmdclass={'build_ext': build_ext}
+    cmdclass={'build_ext': build_ext},
 )
